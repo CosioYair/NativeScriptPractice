44154fa39fce30b8178491c14085f5ade0bbf165
ycosio <brasil_10-9@hotmail.com>
2018-03-16 13:02:50 -0600

    Interface para customer

---
+++ app/interfaces/customer.interface.ts
@@ -0,0 +1,18 @@
+export interface Customer {
+    customerNo: string,
+    addressLine1: string,
+    addressLine2: string,
+    city: string,
+    comment: string,
+    countryCode: string,
+    customerDiscountRate: number,
+    customerName: string,
+    customerStatus: string,
+    customerType: string,
+    dateCreated: string,
+    dateLastActivity: string,
+    dateUpdated: string,
+    state: string,
+    telephoneNo: string,
+    zipCode: string
+}
