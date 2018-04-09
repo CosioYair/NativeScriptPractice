4191923f8c155c6c353e2b8eabcbbc20d398691b
ycosio <brasil_10-9@hotmail.com>
2018-04-09 10:48:00 -0500

    Filtrando por quotes y sales

--- app/components/reviewTransaction/reviewTransaction.component.ts
+++ app/components/reviewTransaction/reviewTransaction.component.ts
@@ -17,11 +17,24 @@ export class ReviewTransactionComponent {
     private _sales: SaleOrder[];
     private _quotes: SaleOrder[];
     public transactionList: any;
+    public userTransactions: any[];
+    public userTransaction: number = 0;
 
     constructor(private _router: Router, private _saleOrderService: SaleOrderService) {
         this._sales = _saleOrderService.getUserSaleOrderUnsaved();
         this._quotes = _saleOrderService.getUserQuoteUnsaved();
-        this.transactionList = new ObservableArray<SaleOrder>(this._sales)
+        this.transactionList = new ObservableArray<SaleOrder>(this._sales);
+        this.userTransactions = ["Sales order", "Quotes"];
+
+    }
+
+    public setUserTransaction() {
+        setTimeout(() => {
+            if (this.userTransaction == 0)
+                this.transactionList = new ObservableArray<SaleOrder>(this._sales);
+            else
+                this.transactionList = new ObservableArray<SaleOrder>(this._quotes);
+        }, 500);
     }
 
     public editTransaction() {
